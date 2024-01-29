dcda1a0a53ce7e3c2c536fe7cc22044f5fb08647
IFE-DREAM <1914175209@qq.com>
2024-01-30 00:20:44 +0800

    first

---
+++ js/data/sets/gen7.js
@@ -0,0 +1,5 @@
+var SETDEX_SM = {
+ "Sharpedo": {
+    "RU Swords Dance": {"ability":"Soundproof","item":"Abomasite","nature":"Adamant","evs":{"at":252,"df":4,"sp":252},"moves":["Swords Dance","Wood Hammer","Ice Shard","Earthquake"]}
+  }
+}
